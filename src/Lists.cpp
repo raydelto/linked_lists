9b75ed2ef13ab98ed3e2f2311413110adfad5174
raydelto <raydelto@gmail.com>
2016-01-06 11:51:17 -0400

    Adding menus and remove capabilities

--- src/Lists.cpp
+++ src/Lists.cpp
@@ -10,20 +10,98 @@
 #include "List.h"
 using namespace std;
 
-int main() {
-	Node* mitch = new Node("Mitch");
-	Node* dustin = new Node("Dustin");
-	Node* ray = new Node("Ray");
-	List* list = new List();
-	list->add(mitch);
-	list->add(dustin);
-	list->add(ray);
+List* list;
+
+void listItems();
+
+
+void initialize(){
+	list = new List();
+}
+
+void remove()
+{
+	listItems();
+	short index;
+	cout << "Please remove the index of the item you'll like to delete: ";
+	cin >> index;
+	Node* node = list->getFirst();
+	for(int i = 1 ; i < index; i++)
+	{
+		node = node ->getNext();
+	}
+	string name = node ->getName();
+	list->remove(node);
+	cout << "Successfully removed the item " << name << "\n\n\n";
+
+}
+
+void add()
+{
+	string name;
+	cout << "Please type the name of the item you'll like to add: ";
+	cin >> name;
+	Node* node = new Node(name);
+	list -> add(node);
+	cout << "Successfuly added the item " << name << " to the list.";
+	cout << "\n\n";
+
+
+}
+
+void listItems()
+{
+	int counter = 1;
 	Node* i = list->getFirst();
 	while(i != NULL)
 	{
-		cout << i -> getName() << endl;
+		cout << counter << " - " << i -> getName() << endl;
 		i = i ->getNext();
+		counter++;
 	}
+	cout << "\n\n\n";
 
+}
+
+
+void displayOptions()
+{
+	short option;
+	do{
+		cout << "1- List items" << endl;
+		cout << "2- Add Item" << endl;
+		cout << "3- Remove Item" << endl;
+		cout << "4- Exit" << endl;
+		cout << "Choose an option => ";
+		cin >> option;
+		switch(option)
+		{
+		case 1:
+			listItems();
+			break;
+		case 2:
+			add();
+			break;
+		case 3:
+			remove();
+			break;
+		case 4:
+			cout << "Good bye" << endl;
+			break;
+		default:
+			cout << "Please select an option between 1 and 4" << endl;
+
+		}
+	}while(option != 4);
+
+}
+
+
+
+
+int main()
+{
+	initialize();
+	displayOptions();
 	return 0;
 }
